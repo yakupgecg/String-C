bbfdc23ce43009888fe8fe50dd7142cfe0942bd8
Yakup <yakup.jarvenpaa@hotmail.com>
2025-05-10 23:23:23 +0300

    Add functions.h header for cstr_t function declarations

---
+++ Headers/functions.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "structure.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Defines functions for handling cstr_t
+
+cstr_t *init_cstr();
+cstr_t *convert_cstr(char *);
+char *convert_nstr(cstr_t *);
