23327a616a2d465e303bf125605fcc37720f8650
Yakup <yakup.jarvenpaa@hotmail.com>
2025-05-11 13:39:09 +0300

    Add function free_cstr()

--- Headers/functions.h
+++ Headers/functions.h
@@ -8,6 +8,7 @@
 // Declares functions for handling cstr_t
 
 cstr_t *init_cstr();
+void free_cstr(cstr_t *);
 unsigned int cstr_length(cstr_t *);
 cstr_t *convert_cstr(char *);
 char *convert_nstr(cstr_t *);
